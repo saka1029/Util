53a254fb7d11ec134dc91f2218f26fb4d7f687af
saka1029 <saka1029@gmail.com>
2020-04-02 07:40:29 +0900

    サンプルバッチファイル追加

---
+++ srt.bat
@@ -0,0 +1,4 @@
+set I="charlies.angels.2019.1080p.bluray.x264-drones.srt"
+set O="Charlie's.Angels.2019.1080p.WEBRip.x264.AAC5.1-[YTS.MX].srt"
+
+java -Dfile.encoding=UTF-8 Srt %I% %O% 00:00:15,283 01:52:37,638 00:00:51,000 01:53:14,000
--- util.bat
+++
@@ -1,5 +0,0 @@
-@setlocal
-
-java -cp d:/git/Util/bin util.%1 %2 %3 %4 %5 %6 %7 %8 %9
-
-@endlocal
